3b04bce064dcc8f44e461b26f6b56a2c59456263
kennydrake47 <kennydrake02@gmail.com>
2023-03-03 01:25:20 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
- *_strcat - concatenates  the string pointed to by @src to
- * the end of the string pointed to by @dest
- *@dest: String that will be appended
- *@src: String to be concatenated upon
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
  *
- * Return: returns poiner to @dest
+ * Return: A pointer to the resulting string dest.
  */
-
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
-
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
-
 	return (dest);
 }
